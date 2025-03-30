d99480bb8d873278f5e5f1289df7b4370e5752dd
suleesh55 <suleesh393@gmail.com>
2025-03-30 13:59:18 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function validatePasswords() {
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+
+    if (password !== confirmPassword) {
+        alert("Passwords do not match!");
+        return false;
+    }
+
+    return true;
+}
